747c85cf41ba911d1b72cae3c485470c94778eb1
Pawel Wydra <wydrapawel@icloud.com>
2023-12-11 11:04:31 +0100

    add folders and html structure

--- src/App.js
+++ src/App.js
@@ -1,44 +1,22 @@
-import Contact from "./Pages/Contact";
-import Home from "./Pages/Home";
-import Login from "./Pages/Login";
-import SignUp from "./Pages/SignUp";
-import Navbar from "./components/Nav/Navbar";
 
-import { Route, Routes } from "react-router-dom";
-import { AuthContextProvider } from "./context/AuthContext";
-import Classes from "./Pages/Classes";
-import Price from "./Pages/Price";
-import Blog from "./Pages/Blog";
-import Schedule from "./Pages/Schedule/Schedule";
-import Monday from "./Pages/Schedule/Monday";
-import Tuesday from "./Pages/Schedule/Tuesday";
-import Wednesday from "./Pages/Schedule/Wednesday";
-import Thursday from "./Pages/Schedule/Thursday";
-import Friday from "./Pages/Schedule/Friday";
-import Saturday from "./Pages/Schedule/Saturday";
-import Sunday from "./Pages/Schedule/Sunday";
-import Gallery from "./Pages/Gallery/Gallery";
-import GalleryPage1 from "./Pages/Gallery/GalleryPage1";
-import GalleryPage2 from "./Pages/Gallery/GalleryPage2";
-import About from "./Pages/About";
 
 function App() {
   return (
   <div className="main-wrapper">
-    <section></section>
-    <section></section>
-    <section></section>
-    <section></section>
-    <section></section>
-    <section></section>
-    <section></section>
-    <section></section>
-    <section></section>
-    <section></section>
-    <section></section>
-    <section></section>
-    <section></section>
-    <section></section>
+    <section>navbar</section>
+    <section>title</section>
+    <section>introduce</section>
+    <section>classes</section>
+    <section>join div</section>
+    <section>choose us</section>
+    <section>trainers</section>
+    <section>TESTIMONIALS</section>
+    <section>GALLERY</section>
+    <section>BMI</section>
+    <section>Pricing</section>
+    <section>BLOG</section>
+    <section>advertisment</section>
+    <section>footer</section>
   </div>
   );
 }
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+    return ( <div className="navbar-container">
+        <div className="navbar-logo"></div>
+        <div className="navbar-menu"></div>
+        <div className="navbar-login"></div>
+    </div> );
+}
+
+export default Navbar;
