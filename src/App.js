ff6db97ae10de189cb5e518dadef60eb1ba2fd7a
Pawel Wydra <wydrapawel@icloud.com>
2023-12-11 12:54:37 +0100

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import "./App.css"
+import Navbar from "./components/navbar/navbar";
 
 function App() {
   return (
   <div className="main-wrapper">
-    <section>navbar</section>
+    <Navbar />
     <section>title</section>
     <section>introduce</section>
     <section>classes</section>
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,9 +1,37 @@
+import "./navbar.css";
+import mainLogo from "../../images/logo/logo.svg";
+import { FaRegUser } from "react-icons/fa";
+import { FaChartBar } from "react-icons/fa6";
+import { FaPlus } from "react-icons/fa6";
+
 function Navbar() {
-    return ( <div className="navbar-container">
-        <div className="navbar-logo"></div>
-        <div className="navbar-menu"></div>
-        <div className="navbar-login"></div>
-    </div> );
+  return (
+    <div className="navbar-container">
+      <div className="navbar-logo">
+        <img src={mainLogo} alt="logo" />
+      </div>
+      <div className="navbar-menu">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Gallery</li>
+          <li>Schedule</li>
+          <li>Blog</li>
+          <li>Pricing</li>
+          <li>Classes</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+      <div className="navbar-login">
+          <FaRegUser className="login-icon-item" />
+          <FaChartBar className="login-icon-item" />
+        <div className="login-join-class">
+          <div className="login-plus"><FaPlus /></div>
+          <h1>JOIN CLASS NOW</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
