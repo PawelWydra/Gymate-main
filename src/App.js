4e1ee815e8fb8df34a39a7a26784a9a47c02bad1
Pawel Wydra <wydrapawel@icloud.com>
2023-12-11 10:47:15 +0100

    init commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import Contact from "./Pages/Contact";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import SignUp from "./Pages/SignUp";
+import Navbar from "./components/Nav/Navbar";
+
+import { Route, Routes } from "react-router-dom";
+import { AuthContextProvider } from "./context/AuthContext";
+import Classes from "./Pages/Classes";
+import Price from "./Pages/Price";
+import Blog from "./Pages/Blog";
+import Schedule from "./Pages/Schedule/Schedule";
+import Monday from "./Pages/Schedule/Monday";
+import Tuesday from "./Pages/Schedule/Tuesday";
+import Wednesday from "./Pages/Schedule/Wednesday";
+import Thursday from "./Pages/Schedule/Thursday";
+import Friday from "./Pages/Schedule/Friday";
+import Saturday from "./Pages/Schedule/Saturday";
+import Sunday from "./Pages/Schedule/Sunday";
+import Gallery from "./Pages/Gallery/Gallery";
+import GalleryPage1 from "./Pages/Gallery/GalleryPage1";
+import GalleryPage2 from "./Pages/Gallery/GalleryPage2";
+import About from "./Pages/About";
+
+function App() {
+  return (
+  <div className="main-wrapper">
+    <section></section>
+    <section></section>
+    <section></section>
+    <section></section>
+    <section></section>
+    <section></section>
+    <section></section>
+    <section></section>
+    <section></section>
+    <section></section>
+    <section></section>
+    <section></section>
+    <section></section>
+    <section></section>
+  </div>
+  );
+}
+
+export default App;
