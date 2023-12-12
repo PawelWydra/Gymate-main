b1a57fc956554d135eaefef55cd14f8a3d3c09ce
Pawel Wydra <wydrapawel@icloud.com>
2023-12-12 08:54:02 +0100

    add css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import "./App.css"
-import Navbar from "./components/navbar/navbar";
+import "./app.css";
+import Navbar from "./components/navbar/navbar.jsx";
+import Title from "./components/title/title.jsx";
 
 function App() {
   return (
-  <div className="main-wrapper">
-    <Navbar />
-    <section>title</section>
-    <section>introduce</section>
-    <section>classes</section>
-    <section>join div</section>
-    <section>choose us</section>
-    <section>trainers</section>
-    <section>TESTIMONIALS</section>
-    <section>GALLERY</section>
-    <section>BMI</section>
-    <section>Pricing</section>
-    <section>BLOG</section>
-    <section>advertisment</section>
-    <section>footer</section>
-  </div>
+    <div className="main-wrapper">
+      <Navbar />
+      <Title />
+      <section>introduce</section>
+      <section>classes</section>
+      <section>join div</section>
+      <section>choose us</section>
+      <section>trainers</section>
+      <section>TESTIMONIALS</section>
+      <section>GALLERY</section>
+      <section>BMI</section>
+      <section>Pricing</section>
+      <section>BLOG</section>
+      <section>advertisment</section>
+      <section>footer</section>
+    </div>
   );
 }
 
---
+++ src/components/title/title.jsx
@@ -0,0 +1,32 @@
+import "./title.css";
+import { FaFacebookF } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa6";
+import { RiTwitterXLine } from "react-icons/ri";
+import titleImage from "../../images/choose-us/title-bg.svg"
+
+function Title() {
+  return (
+    <div className="title-container">
+      <div className="title-content">
+        <div className="title-item">
+          <p>FIND YOUR ENERGY</p>
+          <img src={titleImage} alt="" className="title-image" />
+          <h1>
+            MAKE YOUR BODY
+            <br />
+            <span style={{ fontWeight: 300 }}>FIT & PERFECT</span>
+          </h1>
+          <button className="title-btn">OUR CLASSES</button>
+        </div>
+        <div className="title-social">
+          <FaFacebookF />
+          <FaInstagram />
+          <RiTwitterXLine /> <span className="red-line"></span>
+          <h2 className="vericaltext">SHARE</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Title;
