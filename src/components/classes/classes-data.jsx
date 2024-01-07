13dfdfc6bfbe9d36b7cdff96af9fe96952936b8b
pawel <wydrapawel@icloud.com>
2024-01-07 10:12:30 +0100

    change list into objects

---
+++ src/components/classes/classes-data.jsx
@@ -0,0 +1,17 @@
+import cycling from "../../images/classes/cycling.jpg";
+import karate from "../../images/classes/karate.jpg";
+import powerLifting from "../../images/classes/powerlifting.jpg";
+import meditation from "../../images/classes/meditation.jpg";
+import mma from "../../images/classes/mma.jpg";
+import workout from "../../images/classes/workout.jpg";
+
+const gridItems = {
+    cycling: { name: "Cycling", schedule: "Wednesday: 9:00am-10:00am", image: cycling },
+    karate: { name: "Karate", schedule: "Friday: 10:00am-11:00am", image: karate },
+    powerLifting: { name: "Power", schedule: "Saturday: 9:00am-10:00am", image: powerLifting },
+    meditation: { name: "Meditation", schedule: "Friday: 1:00pm-2:00pm", image: meditation },
+    mma: { name: "Martial Arts", schedule: "Sunday: 6:00pm-7:00pm", image: mma },
+    workout: { name: "Workout", schedule: "Monday: 4:00pm-5:00pm", image: workout },
+  };
+
+export default gridItems;
--- src/components/classes/classes.jsx
+++ src/components/classes/classes.jsx
@@ -1,21 +1,7 @@
 import "./classes.css";
 import redBg from "../../images/choose-us/title-bg.svg";
 import GridItem from "./grid-item/grid-item";
-import cycling from "../../images/classes/cycling.jpg";
-import karate from "../../images/classes/karate.jpg";
-import powerLifting from "../../images/classes/powerlifting.jpg";
-import meditation from "../../images/classes/meditation.jpg";
-import mma from "../../images/classes/mma.jpg";
-import workout from "../../images/classes/workout.jpg";
-
-const gridItems = [
-  ["Cycling", "Wednesday: 9:00am-10:00am", cycling],
-  ["Karate", "Friday: 10:00am-11:00am", karate],
-  ["Power", "Saturday: 9:00am-10:00am", powerLifting],
-  ["Meditation", "Friday: 1:00pm-2:00pm", meditation],
-  ["Martial Arts", "Sunday: 6:00pm-7:00pm", mma],
-  ["Workout", "Monday: 4:00pm-5:00pm", workout],
-];
+import gridItems from "./classes-data.jsx";
 
 function Classes() {
   return (
@@ -27,14 +13,17 @@ function Classes() {
         </div>
         <h1>We Are Offering Best Flexible Classes</h1>
         <div className="classes-image-grid">
-          {gridItems.map(([activity, time, backgroundImage], index) => (
-            <GridItem
-              key={index}
-              activity={activity}
-              time={time}
-              backgroundImage={backgroundImage}
-            />
-          ))}
+          {Object.keys(gridItems).map((key, index) => {
+            const { name, schedule, image } = gridItems[key];
+            return (
+              <GridItem
+                key={index}
+                activity={name}
+                time={schedule}
+                backgroundImage={image}
+              />
+            );
+          })}
         </div>
       </div>
     </div>
