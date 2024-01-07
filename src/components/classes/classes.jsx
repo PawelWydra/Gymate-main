7c62b7ccfccbf139bdf9f3a86a9c086fba913f74
pawel <wydrapawel@icloud.com>
2024-01-07 09:59:59 +0100

    add greyscale effect

--- src/components/classes/classes.jsx
+++ src/components/classes/classes.jsx
@@ -1,5 +1,21 @@
 import "./classes.css";
 import redBg from "../../images/choose-us/title-bg.svg";
+import GridItem from "./grid-item/grid-item";
+import cycling from "../../images/classes/cycling.jpg";
+import karate from "../../images/classes/karate.jpg";
+import powerLifting from "../../images/classes/powerlifting.jpg";
+import meditation from "../../images/classes/meditation.jpg";
+import mma from "../../images/classes/mma.jpg";
+import workout from "../../images/classes/workout.jpg";
+
+const gridItems = [
+  ["Cycling", "Wednesday: 9:00am-10:00am", cycling],
+  ["Karate", "Friday: 10:00am-11:00am", karate],
+  ["Power", "Saturday: 9:00am-10:00am", powerLifting],
+  ["Meditation", "Friday: 1:00pm-2:00pm", meditation],
+  ["Martial Arts", "Sunday: 6:00pm-7:00pm", mma],
+  ["Workout", "Monday: 4:00pm-5:00pm", workout],
+];
 
 function Classes() {
   return (
@@ -11,17 +27,14 @@ function Classes() {
         </div>
         <h1>We Are Offering Best Flexible Classes</h1>
         <div className="classes-image-grid">
-          <div className="classes-image-item">
-            <div className="classes-icon-container">
-              <img src="" alt="" />
-            </div>
-            <div className="classes-description-container">
-              <h2>Cycling</h2>
-              <div className="classes-date-container">
-                <p>Wednesday: 9:00am-10:00am</p>
-              </div>
-            </div>
-          </div>
+          {gridItems.map(([activity, time, backgroundImage], index) => (
+            <GridItem
+              key={index}
+              activity={activity}
+              time={time}
+              backgroundImage={backgroundImage}
+            />
+          ))}
         </div>
       </div>
     </div>
--- src/components/classes/grid-item/grid-item.jsx
+++ src/components/classes/grid-item/grid-item.jsx
@@ -1,17 +1,26 @@
-import './grid-item.css';
+import "./grid-item.css";
 
 function GridItem(props) {
-    return ( <div className="classes-image-item">
-    <div className="classes-icon-container">
-      <img src="" alt="" />
-    </div>
-    <div className="classes-description-container">
-      <h2>{props.title}</h2>
-      <div className="classes-date-container">
-        <p>{props.date}</p>
+  const { backgroundImage } = props;
+
+  const containerStyle = {
+    background: `url(${backgroundImage}) top/cover no-repeat`,
+  };
+
+  return (
+    <div className="image-item-wrapper">
+      <div style={containerStyle} className="classes-image-item"></div>
+      <div className="classes-icon-container">
+        <img src="" alt="" />
+      </div>
+      <div className="classes-description-container">
+        <h2>{props.activity}</h2>
+        <div className="classes-date-container">
+          <p>{props.time}</p>
+        </div>
       </div>
     </div>
-  </div> );
+  );
 }
 
 export default GridItem;
