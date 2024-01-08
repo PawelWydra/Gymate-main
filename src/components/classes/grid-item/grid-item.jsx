f73dffbe011d1689f300b9ca96349f48815b028d
pawel <wydrapawel@icloud.com>
2024-01-08 10:24:58 +0100

    add icons to grid

--- src/components/classes/grid-item/grid-item.jsx
+++ src/components/classes/grid-item/grid-item.jsx
@@ -1,17 +1,18 @@
 import "./grid-item.css";
+import bike from "../../../images/features/bike-red.png"
 
 function GridItem(props) {
   const { backgroundImage } = props;
 
   const containerStyle = {
-    background: `url(${backgroundImage}) top/cover no-repeat`,
+    background: `url(${backgroundImage}) center/cover no-repeat`,
   };
 
   return (
     <div className="image-item-wrapper">
       <div style={containerStyle} className="classes-image-item"></div>
       <div className="classes-icon-container">
-        <img src="" alt="" />
+        <img src={bike} alt="" />
       </div>
       <div className="classes-description-container">
         <h2>{props.activity}</h2>
