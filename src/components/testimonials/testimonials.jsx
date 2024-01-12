157fc4c0dccd4567c9e088d3fa23206699d2bc6f
pawel <wydrapawel@icloud.com>
2024-01-12 15:16:51 +0100

    add folders and jsx file with html structure

---
+++ src/components/testimonials/testimonials.jsx
@@ -0,0 +1,28 @@
+const paragraphText = `I've been a member of Gymate for the past 6 months and it has
+been an amazing experience. The trainers are knowledgeable and
+supportive, the equipment is top-notch, and the community of
+members is friendly and encouraging.`;
+
+function Testimonials() {
+  return (
+    <div className="main-container">
+      <div className="h2-title"></div>
+      <div className="main-wrapper">
+        <div className="context-wrapper">
+          <img src="" alt="" className="grazka" />
+          <div className="harry-potter-text">
+            <img src="" alt="" className="cudzyslow" />
+            <p>{paragraphText}</p>
+            <h1>Harry Potter</h1>
+            <p>CEO of Hogwarts</p>
+          </div>
+          <div className="arrow-btn-cnt">
+            <button></button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Testimonials;
