3082d81ef678a7300f76134806def50e3238e3f8
pawel <wydrapawel@icloud.com>
2024-01-12 15:05:22 +0100

    trainer card added

--- src/components/trainers/trainerCard/trainerCard.jsx
+++ src/components/trainers/trainerCard/trainerCard.jsx
@@ -1,15 +1,31 @@
-function TrainerCard() {
+import trainerBg from "../../../images/trainers/trainer-bg.png";
+import { FaFacebookF } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa6";
+import { RiTwitterXLine } from "react-icons/ri";
+import { AiOutlineMail } from "react-icons/ai";
+
+function TrainerCard({ trainer, name, specialization }) {
+  const trainerClasses = `
+    absolute left-12 top-0 z-1
+    w-56 grayscale duration-300 ease-in-out hover:grayscale-0
+  `;
+
   return (
-    <div className="trainer-card">
-      <div className="trainer-img-container">
-        <img src="" alt="" className="trainer-img" />
-        <img src="" alt="" className="trainer-background" />
+    <div className="flex flex-col justify-center items-center h-96 mb-5 group">
+      <div className="relative h-72 w-80 overflow-hidden">
+        <img src={trainerBg} alt="" className="absolute top-12" />
+        <img src={trainer} alt="" className={trainerClasses} />
       </div>
-      <div className="trainer description-container">
-        <h2>John Levis</h2>
-        <p>Yoga Trainer</p>
-        <div className="trainer-social-media-icons">fa, x, instagram, mail</div>
-        <span className="trainer-red-line"></span>
+      <div className="flex flex-col justify-center items-center w-80 h-40 gap-2 shadow-lg shadow-black/20">
+        <h2 className="text-2xl font-semibold mt-auto">{name}</h2>
+        <p className="text-center max-w-md text-gray-500">{specialization}</p>
+        <div className="flex gap-5 text-gray-500">
+          <FaFacebookF className="w-4 h-4" />
+          <FaInstagram className="w-4 h-4" />
+          <RiTwitterXLine className="w-4 h-4" />
+          <AiOutlineMail className="w-4 h-4" />
+        </div>
+        <span className="bg-red-500 h-1 w-10 mt-auto group-hover:w-full transition-all duration-700"></span>
       </div>
     </div>
   );
--- src/components/trainers/trainers.jsx
+++ src/components/trainers/trainers.jsx
@@ -1,19 +1,49 @@
 import "./trainers.css";
 import TrainerCard from "./trainerCard/trainerCard.jsx";
 import RedBgHeading from "../title/redBgHeading/redBgHeading";
+import johnLevis from "../../images/trainers/trainer1.png";
+import johnatanDoe from "../../images/trainers/trainer2.png";
+import anaJune from "../../images/trainers/trainer3.png";
+
+const trainerArray = [
+  {
+    image: johnLevis,
+    name: "John Levis",
+    specialization: "Yoga Trainer",
+  },
+  {
+    image: johnatanDoe,
+    name: "Johnatan Doe",
+    specialization: "Fitness Instructor",
+  },
+  {
+    image: anaJune,
+    name: "Ana June",
+    specialization: "Pilates Instructor",
+  },
+];
 
 function Trainers() {
   return (
     <div className="trainers-container">
-      <div className="trainers-wrapper">
+      <div className="trainers-wrapper my-24 text-center">
         <RedBgHeading text={"GYM TRAINERS"} />
-        <h1>Team Of Expert Coaches</h1>
-        <p>
+        <h1 className="text-4xl font-semibold max-w-lg">
+          Team Of Expert Coaches
+        </h1>
+        <p className="max-w-lg text-gray-500">
           Expert team of coaches helps you succeed in any goal, personalized
           guidance and motivation provided!
         </p>
-        <div className="trainer-card-container">
-          <TrainerCard />
+        <div className="flex flex-col gap-16 md:flex-row">
+          {trainerArray.map((trainer, index) => (
+            <TrainerCard
+              key={index}
+              trainer={trainer.image}
+              name={trainer.name}
+              specialization={trainer.specialization}
+            />
+          ))}
         </div>
       </div>
     </div>
