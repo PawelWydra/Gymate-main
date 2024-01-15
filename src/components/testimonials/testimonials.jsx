176955f9797438b331ec55ce83e193609937f125
pawel <wydrapawel@icloud.com>
2024-01-15 12:44:10 +0100

    add testimonials

--- src/components/testimonials/testimonials.jsx
+++ src/components/testimonials/testimonials.jsx
@@ -1,30 +1,45 @@
 import RedBgHeading from "../title/redBgHeading/redBgHeading";
+import quotes from "../../images/testimonials/quotes.png";
+import testimonialNew from "../../images/testimonials/testimonial-new.jpg";
+import ArrowRedBtn from "./arrowRedBtn/arrowRedBtn";
 
-const paragraphText = `I've been a member of Gymate for the past 6 months and it has
+const paragraphText = `"I've been a member of Gymate for the past 6 months and it has
 been an amazing experience. The trainers are knowledgeable and
 supportive, the equipment is top-notch, and the community of
-members is friendly and encouraging.`;
+members is friendly and encouraging."`;
 
-function Testimonials() {
+const Testimonials = () => {
+  const ladyStyles = {
+    position: "absolute",
+    left: 0,
+    bottom: 0,
+    maxWidth: "35%",
+    height: "30rem",
+    borderTopRightRadius: "50%",
+  };
+
   return (
-    <div className="flex flex-col items-center gap-4 w-full">
-      <RedBgHeading text={"TESTIMONIALS"} width={"15rem"} />
-      <div className="flex items-center justify-center size-10/12 border-8 border-gray-100 p-8">
-        <div className="flex items-center justify-center size-max p-8 bg-black text-white">
-          <img src="" alt="" className="grazka" />
-          <div className="harry-potter-text">
-            <img src="" alt="" className="cudzyslow" />
-            <p>{paragraphText}</p>
-            <h1>Harry Potter</h1>
-            <p>CEO of Hogwarts</p>
+    <div className="flex flex-col items-center gap-40 w-full">
+      <RedBgHeading text="TESTIMONIALS" width="15rem" />
+      <div className="flex items-center justify-center max-w-6xl border-8 border-gray-200 p-8">
+        <div className="flex justify-end size-max p-8 bg-black text-white relative">
+          <img src={testimonialNew} alt="" style={ladyStyles} />
+          <div className="w-7/12 h-2/3 flex flex-col">
+            <img src={quotes} alt="" className="size-16" />
+            <p className="w-10/12 my-4 text-lg font-thin italic text-gray-300">
+              {paragraphText}
+            </p>
+            <h1 className="text-2xl font-semibold">Harry Potter</h1>
+            <p className="font-thin text-sm">CEO of Hogwarts</p>
           </div>
-          <div className="arrow-btn-cnt">
-            <button></button>
+          <div className="absolute bottom-6 right-6 flex justify-center gap-2">
+            <ArrowRedBtn className />
+            <ArrowRedBtn additionalClassName="transform rotate-180" />
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Testimonials;
