5a34e15d50e02cfb13c98f791e232234e60c817e
pawel <wydrapawel@icloud.com>
2024-01-15 16:02:47 +0100

    add files

---
+++ src/components/gallery/gallery.jsx
@@ -0,0 +1,13 @@
+function Gallery() {
+  return (
+    <div className="main-container">
+      <div className="main-wrapper">
+        <h2></h2>
+        <h1></h1>
+        <div className="gallery-container"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Gallery;
