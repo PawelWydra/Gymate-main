bb892d6ab8fb6000a5617a72ee1c1c084a9f5147
pawel <wydrapawel@icloud.com>
2024-01-16 12:57:13 +0100

    feat: gallery section done

--- src/components/gallery/gallery.jsx
+++ src/components/gallery/gallery.jsx
@@ -3,19 +3,61 @@ import barbleGirl from "../../images/gallery/img2.jpg";
 import linesGirl from "../../images/gallery/img3.jpg";
 import absGuy from "../../images/gallery/img4.jpg";
 import plankGirl from "../../images/gallery/img5.jpg";
-
+import RedBgHeading from "../title/redBgHeading/redBgHeading";
 
 function Gallery() {
+  const boxingGirlStyle = {
+    backgroundImage: `url(${boxingGirl})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  };
+
+  const barbleGirlStyle = {
+    backgroundImage: `url(${barbleGirl})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  };
+
+  const linesGirlStyle = {
+    backgroundImage: `url(${linesGirl})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  };
+
+  const absGuyStyle = {
+    backgroundImage: `url(${absGuy})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  };
+
+  const plankGirlStyle = {
+    backgroundImage: `url(${plankGirl})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  };
+
   return (
-    <div className="main-container">
-      <div className="main-wrapper">
-        <h2></h2>
-        <h1>Our Workplace Gallery</h1>
-        <p>
+    <div className="w-full flex flex-col items-center bg-white p-36">
+      <div className="max-w-6xl flex flex-col items-center gap-6">
+        <RedBgHeading text={"GYM GALLERY"} />
+        <h1 className="text-4xl font-semibold text-black">
+          Our Workplace Gallery
+        </h1>
+        <p className="w-8/12 text-gray-500 text-center">
           Our Workplace Gallery features modern office, team collaboration, and
-          fun culture. Attracts talents and showcases company's work atmosphere.
+          fun culture. Attracts talents and showcases the company's work
+          atmosphere.
         </p>
-        <div className="gallery-container"></div>
+        <div
+          className="w-full grid grid-rows-2 grid-cols-4 gap-2"
+          style={{ height: "40rem" }}
+        >
+          <div className="col-span-2 row-span-2" style={boxingGirlStyle}></div>
+          <div className="col-span-2" style={barbleGirlStyle}></div>
+          <div className="col-span-2 row-span-2" style={plankGirlStyle}></div>
+          <div className="h-48" style={linesGirlStyle}></div>
+          <div className="" style={absGuyStyle}></div>
+        </div>
       </div>
     </div>
   );
