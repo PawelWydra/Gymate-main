f7a50ba458528f08047459bdd94ccac036a60248
pawel <wydrapawel@icloud.com>
2024-01-20 15:42:10 +0100

    feat: add rwd to testimonials

--- src/components/testimonials/testimonials.jsx
+++ src/components/testimonials/testimonials.jsx
@@ -3,6 +3,7 @@ import quotes from "../../assets/images/testimonials/quotes.png";
 import testimonialNew from "../../assets/images/testimonials/testimonial-new.jpg";
 import ArrowRedBtn from "./arrowRedBtn/arrowRedBtn";
 import LogoContainer from "./logoContainer/logoContainer";
+import SectionContainer from "../sectionContainer/sectionContainer";
 
 const paragraphText = `I've been a member of Gymate for the past 6 months and it has
 been an amazing experience. The trainers are knowledgeable and
@@ -20,12 +21,18 @@ const Testimonials = () => {
   };
 
   return (
-    <div className="flex flex-col items-center mt-20 w-full">
-      <RedBgHeading text="TESTIMONIALS" width="15rem" />
-      <div className="flex items-center justify-center max-w-6xl mt-40 border-8 border-gray-200 p-8">
-        <div className="flex justify-end size-max p-8 bg-black text-white relative">
-          <img src={testimonialNew} alt="" style={ladyStyles} />
-          <div className="w-7/12 h-2/3 flex flex-col">
+    <SectionContainer bg={"#fff"}>
+      <div className="mx-auto">
+        <RedBgHeading text="TESTIMONIALS" width="15rem" />
+      </div>
+      <div className="flex items-center justify-center max-w-6xl mt-10 md:mt-15 2xl:mt-40 border-8 border-gray-200 p-8">
+        <div className="flex justify-end p-8 bg-black text-white relative">
+          <img
+            className="hidden lg:block absolute left-0 bottom-0 rounded-tr-[50%] min-h-full max-h-full 2xl:max-h-fit w-5/12 3xl:w-[27rem]"
+            src={testimonialNew}
+            alt=""
+          />
+          <div className="lg:w-6/12 h-2/3 flex flex-col">
             <img src={quotes} alt="" className="size-16" />
             <q className="w-10/12 my-4 text-lg font-thin italic text-gray-300">
               {paragraphText}
@@ -41,7 +48,7 @@ const Testimonials = () => {
       </div>
       <LogoContainer />
       <hr className="border-t border-gray-300 w-full" />
-    </div>
+    </SectionContainer>
   );
 };
 
