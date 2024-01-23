e555b6be86aef01890f1b42b4848285d092626ec
pawel <wydrapawel@icloud.com>
2024-01-23 18:34:33 +0100

    fix: resize img element in grid

--- src/components/classes/grid-item/grid-item.jsx
+++ src/components/classes/grid-item/grid-item.jsx
@@ -3,13 +3,11 @@ import "./grid-item.css";
 function GridItem(props) {
   const { backgroundImage } = props;
 
-  const containerStyle = {
-    background: `url(${backgroundImage}) center/cover no-repeat`,
-  };
-
   return (
     <div className="image-item-wrapper">
-      <div className="classes-image-item" style={containerStyle}></div>
+      <div className="classes-image-item">
+        <img className="size-full" src={backgroundImage} alt="" />
+      </div>
       <div className="classes-icon-container">
         <img src={props.icon} alt="" />
       </div>
