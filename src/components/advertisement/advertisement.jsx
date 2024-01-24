8c8e4cc6ec8df81f4e687219cb612b67d9574ca7
pawel <wydrapawel@icloud.com>
2024-01-24 15:37:03 +0100

    chow: add style to adv section

--- src/components/advertisement/advertisement.jsx
+++ src/components/advertisement/advertisement.jsx
@@ -1,19 +1,22 @@
-import BtnWithRightArrow from "../title/btnWithRedArrow/btnWithRedArrow.jsx";
+import BtnWithRedArrow from "../title/btnWithRedArrow/btnWithRedArrow.jsx";
 import SectionContainer from "../sectionContainer/sectionContainer";
 import bg from "../../assets/images/call/background.jpg";
 
 function Advertisement() {
   return (
     <SectionContainer bg={bg}>
-      <div className="flex justify-around items-center max-w-5xl min-h-28">
-        <div className="text-xl sm:text-3xl lg:text-4xl font-semibold">
-          <h1 className="">
-            We Are Always Providing Best
-            <br /> Fitness Service For You
-          </h1>
+        <div className="text-4xl xl:text-5xl font-semibold py-5">
+          <h1 className="">Need a Fitness Trainer? </h1>
+          <h2 className="text-3xl my-2"><span className="text-red-500">Call:</span> +123-456789</h2>
+          <div className="mt-10">
+            <BtnWithRedArrow
+              text="PURCHASE NOW"
+              bgColor="bg-buttonColor"
+              textColor="white"
+              arrowColor="white"
+            />
+          </div>
         </div>
-        <BtnWithRightArrow text={"JOIN WITH US"} />
-      </div>
     </SectionContainer>
   );
 }
