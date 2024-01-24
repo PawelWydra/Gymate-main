a7241afa7a37878f0bdaa90de23de2202c0637fc
pawel <wydrapawel@icloud.com>
2024-01-24 16:44:10 +0100

    chore: add column in footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,24 +1,27 @@
 import SectionContainer from "../sectionContainer/sectionContainer.jsx";
 import logo from "../../assets/images/logo/logo-footer.svg";
 import SocialIcon from "./socialIcon/socialIcon.jsx";
-import { socialIcons, paragraph } from "./footerData.jsx"
+import { socialIcons, paragraph } from "./footerData.jsx";
 
 function Footer() {
   return (
     <SectionContainer bg="#fff">
-      <div className="container text-black">
-        <div className="flex flex-col gap-3 w-1/4">
-          <img src={logo} alt="" />
-          <p>{paragraph}</p>
-          <div className="flex gap-2">
+      <div className="flex flex-col md:flex-row gap-10 text-gray-500 tracking-wide font-sans">
+        <div className="flex flex-col gap-6 basis-1/2 ">
+          <img className="w-1/2 lg:w-1/3" src={logo} alt="" />
+          <p className="md:max-w-[25rem]">{paragraph}</p>
+          <div className="flex gap-4">
             {socialIcons.map((socialIcon, index) => (
               <SocialIcon key={index} icon={socialIcon.icon} />
             ))}
           </div>
-          <span className="rights"></span>
-          <span className="rights"></span>
+          <div className="dec">
+            {" "}
+            <p className="rights">Privacy Policy | © 2024 Gymate</p>
+            <p className="rights">Design by RadiusTheme</p>
+          </div>
         </div>
-        <div className="item">
+        <div className="basis-1/4">
           <h1></h1>
           <span></span>
           <p></p>
@@ -27,7 +30,7 @@ function Footer() {
           <p></p>
           <p></p>
         </div>
-        <div className="item">
+        <div className="basis-1/4">
           <h1></h1>
           <span></span>
           <p></p>
