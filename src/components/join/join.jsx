7240a3de3f9b95739c19ec20eb8332a443d0bca4
pawel <wydrapawel@icloud.com>
2024-01-27 10:46:29 +0100

    refactor: add flex property to h1 and button

--- src/components/join/join.jsx
+++ src/components/join/join.jsx
@@ -5,14 +5,15 @@ import bg from "../../assets/images/features/cta-bg.png";
 function Join() {
   return (
     <SectionContainer bg={bg}>
-      <div className="flex justify-around items-center max-w-5xl min-h-28">
-        <div className="text-xl sm:text-3xl lg:text-4xl font-semibold">
+      <div className="flex flex-wrap items-center min-h-28 gap-5 lg:gap-0">
+        <div className="flex-[1_3_0%] text-center text-2xl sm:text-3xl lg:text-4xl font-semibold min-w-fit">
           <h1 className="">
             We Are Always Providing Best
             <br /> Fitness Service For You
           </h1>
         </div>
-        <BtnWithRightArrow text={"JOIN WITH US"} />
+        <div className="flex-[1_2] min-w-80 flex justify-center"> <BtnWithRightArrow text={"JOIN WITH US"} /></div>
+       
       </div>
     </SectionContainer>
   );
