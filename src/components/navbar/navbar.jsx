ec907cb2e1c8e14b4e87e4f3824a576269422e02
pawel <wydrapawel@icloud.com>
2024-01-29 09:02:17 +0100

    refactor: make nav responsive

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -3,33 +3,39 @@ import mainLogo from "../../assets/images/logo/logo.svg";
 import { FaRegUser } from "react-icons/fa";
 import { FaChartBar } from "react-icons/fa6";
 import { FaPlus } from "react-icons/fa6";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 function Navbar() {
+  const menuItems = [
+    "Home",
+    "About",
+    "Gallery",
+    "Schedule",
+    "Blog",
+    "Pricing",
+    "Classes",
+    "Contact",
+  ];
+
   return (
     <div className="navbar-container">
-      <div className="min-w-40 ml-4">
+      <div className="">
         <img src={mainLogo} alt="logo" />
       </div>
-      <div className="navbar-menu">
-        <ul className="flex">
-          <li>Home</li>
-          <li>About</li>
-          <li>Gallery</li>
-          <li>Schedule</li>
-          <li>Blog</li>
-          <li>Pricing</li>
-          <li>Classes</li>
-          <li>Contact</li>
-        </ul>
-      </div>
-      <div className="navbar-login">
+      <ul className="hidden lg:flex w-1/2 justify-around">
+        {menuItems.map((item, index) => (
+          <li key={index} className="">{item}</li>
+        ))}
+      </ul>
+      <div className="navbar-login ">
+        <GiHamburgerMenu size={30} className="lg:hidden" />
         <FaRegUser className="login-icon-item" />
         <FaChartBar className="login-icon-item" />
         <div className="login-join-class">
           <div className="login-plus">
             <FaPlus />
           </div>
-          <h1>JOIN CLASS NOW</h1>
+          <h1 className="hidden md:block">JOIN CLASS NOW</h1>
         </div>
       </div>
     </div>
--- src/components/title/title.jsx
+++ src/components/title/title.jsx
@@ -7,21 +7,21 @@ import BtnWithRedArrow from "./btnWithRedArrow/btnWithRedArrow";
 
 function Title() {
   return (
-    <div className="title-container">
+    <div className="title-container justify-center lg:justify-end">
       <div className="title-content">
-        <div className="title-item max-w-screen-md">
+        <div className="title-item items-center md:items-start">
           <p>FIND YOUR ENERGY</p>
           <img src={titleImage} alt="" className="title-image" />
-          <h1 className="text-5xl">
+          <h1 className="text-5xl leading-snug md:text-left font-semibold">
             MAKE YOUR BODY
             <br />
-            <span style={{ fontWeight: 300 }}>FIT & PERFECT</span>
+            <span className="font-[300]">FIT & PERFECT</span>
           </h1>
           <div className="btn-container">
             <BtnWithRedArrow text={"OUR CLASSES"} />
           </div>
         </div>
-        <div className="title-social">
+        <div className="hidden lg:flex flex-col-reverse items-center gap-6 mr-16">
           <FaFacebookF />
           <FaInstagram />
           <RiTwitterXLine /> <span className="red-line"></span>
